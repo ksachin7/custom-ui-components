e58d816f513e1b4c5a71269e2c21cb4477f74bce
ksachin7 <isachin.imp@gmail.com>
2024-03-31 18:50:09 +0530

    added unit tests

---
+++ src/tests/setupTests.js
@@ -0,0 +1,7 @@
+import Enzyme, { configure, shallow, mount, render } from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+
+configure({ adapter: new Adapter() });
+
+export { shallow, mount, render };
+export default Enzyme;
